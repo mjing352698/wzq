f859f63014efe35d61835c5ac97d696b99a63f9c
mjing352698 <17856113037@163.com>
2026-05-24 23:08:28 +0800

    练习1
    联系1提交

---
+++ lianxi1/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <Father />
+</template>
+<script setup>
+import Father from '../../lianxi3/src/views/father.vue'
+</script>
+@media (min-width: 1024px) {
+  body {
+    display: flex;
+    place-items: center;
+  }
+  #app {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 0 2rem;
+  }
+}
+display: flex;
+    justify-content: center;
+    align-items: center;
